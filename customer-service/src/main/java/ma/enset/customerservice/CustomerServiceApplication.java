5d9bbf8bd13bbcf44f396b5fa18660e140c0e47a
khadija804 <khadija.agoujim17@gmail.com>
2026-06-11 13:57:53 +0100

    create customer and inventory service

---
+++ customer-service/src/main/java/ma/enset/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,26 @@
+package ma.enset.customerservice;
+
+import ma.enset.customerservice.entities.Customer;
+import ma.enset.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.save(Customer.builder().nom("Khadija").email("khadija@gmail.com").build());
+            customerRepository.save(Customer.builder().nom("imane").email("imane@gmail.com").build());
+            customerRepository.save(Customer.builder().nom("mouna").email("mouna@gmail.com").build());
+
+        };
+    }
+}
---
+++ inventory-service/src/main/java/ma/enset/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,24 @@
+package ma.enset.inventoryservice;
+
+import ma.enset.inventoryservice.entities.Product;
+import ma.enset.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository){
+        return args -> {
+            productRepository.save(Product.builder().name("laptop").price(12000).quantity(12).build());
+            productRepository.save(Product.builder().name("phone").price(6000).quantity(120).build());
+        };
+    }
+}
